c4a8fb0d37859c96697613c8b0e551cdb3e92af8
Abenezer Amare <75203583+Ebenezer-A@users.noreply.github.com>
2022-10-06 19:24:14 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { ChatEngine } from "react-chat-engine";
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Hello World</h4>
+    </div>
+  );
+}
+
+export default App;
