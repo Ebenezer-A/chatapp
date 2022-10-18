8680bf8105c13ea135222933936689ac4455c274
Abenezer Amare <abenezerame@gmail.com>
2022-10-18 16:25:36 +0000

    chatengine component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./styles.css";
 import { ChatEngine } from "react-chat-engine";
-
+import ChatFeed from "./components/ChatFeed";
+import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h4>Hello World</h4>
+      <ChatEngine
+        height="100vh"
+        projectID="b0e59ad1-d778-49e3-aec6-c2ec8f461c58"
+        userName="john"
+        userSecret="qwert"
+        renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps  } />}
+        onNewmessage = {() =>
+          new Audio("hhtps://chat-engine-assets.s3.amazonaws.com/click.mp3")
+        }
+      />
     </div>
   );
 }
